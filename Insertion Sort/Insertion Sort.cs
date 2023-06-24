e6ef18d4eaf29dac0a17e3a487e36ac60fd47706
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2023-06-24 11:52:42 +0300

    Add project files.

---
+++ Insertion Sort/Insertion Sort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Insertion_Sort
+{
+    public static class InsertionSort
+    {
+        public static List<int> sort(List<int> list)   // complexity analysis
+        {
+            for (int i = 1; i < list.Count; i++)                //n
+            {
+                int key = list[i];                              //n 
+                int j = i - 1;                                  //n
+                while (j >= 0 && list[j] > key)                //n
+                {
+                    list[j + 1] = list[j];                    //n
+                    j--;                                      //n
+                }
+                list[j + 1] = key;                           //n  
+            }
+            return list;                                     //1 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Algorithms.Insertion_Sort;
+using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Insertion Sort
+            List<int> list = new List<int>() { 9, 5, 1, 4, 3 };
+            list = InsertionSort.sort(list);
+            foreach (var item in list)
+                Console.Write(item + " ");
+            #endregion
+
+        }
+    }
+}
