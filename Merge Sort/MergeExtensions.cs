010d2e6db48369348d59cf54831125b9c97d99b9
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2023-06-24 19:55:46 +0300

    Merge Application : Segeregation between Positive and Negative Numbers at Same Order

---
+++ Merge Sort/MergeExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Merge_Sort
+{
+    public static class MergeExtensions
+    {
+        public static void segeregatePosNegNumbers(List<int> list)
+        {
+            segeregate(list, 0, list.Count - 1);
+        }
+
+        private static void merge(List<int> list, int start, int mid, int end)
+        {
+            int i, j, k;
+            int left_length = mid - start + 1;
+            int right_length = end - mid;
+            int[] left_array = new int[left_length];
+            int[] right_array = new int[right_length];
+
+            for (i = 0; i < left_length; i++)
+            {
+                left_array[i] = list[start + i];
+            }
+            for (j = 0; j < right_length; j++)
+            {
+                right_array[j] = list[mid + 1 + j];
+            }
+            i = 0;
+            j = 0;
+            k = start;
+            while (i < left_length && left_array[i] <= 0)
+            {
+                list[k++] = left_array[i++];
+            }
+            while (j < right_length && right_array[j] <= 0)
+            {
+                list[k++] = right_array[j++];
+            }
+            while (i < left_length )
+            {
+                list[k++] = left_array[i++];
+            }
+            while (j < right_length)
+            {
+                list[k++] = right_array[j++];
+            }
+        }
+
+        private static void segeregate(List<int> list, int start, int end)
+        {
+            if (end <= start)
+                return;
+            int mid = (start + end) / 2;
+            segeregate(list, start, mid);
+            segeregate(list, mid + 1, end);
+            merge(list, start, mid, end);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,27 +13,19 @@ namespace Algorithms
             #region Insertion Sort
             List<int> list = new List<int>() { 9, 5, 1, 4, 3 };
             Console.WriteLine("Before Insertion Sort");
-            foreach (var item in list)
-                Console.Write(item + " ");
-            Console.WriteLine();
+            Console.WriteLine(String.Join(" ", list));
             InsertionSort.Sort(list);
             Console.WriteLine("After Insertion Sort");
-            foreach (var item in list)
-                Console.Write(item + " ");
-            Console.WriteLine();
+            Console.WriteLine(String.Join(" ", list));
             #endregion
             Console.WriteLine("--------------------------------------------------");
             #region Merge Sort
             List<int> mergelist = new List<int>() { 9, 5, 1, 4, 3 };
             Console.WriteLine("Before Merge Sort");
-            foreach (var item in mergelist)
-                Console.Write(item + " ");
-            Console.WriteLine();
+            Console.WriteLine(String.Join(" ", mergelist));
             MergeSort.Sort(mergelist);
             Console.WriteLine("After Merge Sort");
-            foreach (var item in mergelist)
-                Console.Write(item + " ");
-            Console.WriteLine();
+            Console.WriteLine(String.Join(" ", mergelist));
             #endregion
             Console.WriteLine("--------------------------------------------------");
             #region Binary Search
@@ -42,6 +34,13 @@ namespace Algorithms
             Console.WriteLine(BinarySearch.Search(searchlist, 1));
             Console.WriteLine(BinarySearch.Search(searchlist, 17));
             Console.WriteLine(BinarySearch.Search(searchlist, 30));
+            #endregion
+            Console.WriteLine("--------------------------------------------------");
+            #region Segeragation between Positive nd negative numbers 
+            List<int> segList = new List<int>() { 6, -5, 12, 10, -9, -1 };
+            Console.WriteLine(String.Join(" ", segList));
+            MergeExtensions.segeregatePosNegNumbers(segList);
+            Console.WriteLine(String.Join(" ", segList));
             #endregion
         }
     }
