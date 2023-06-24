15cba24149e083fe8b15c54e0f0fe7fe6b055428
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2023-06-24 11:55:24 +0300

    Complexity analysis of insertion sort

--- Insertion Sort/Insertion Sort.cs
+++ Insertion Sort/Insertion Sort.cs
@@ -8,16 +8,16 @@ namespace Algorithms.Insertion_Sort
 {
     public static class InsertionSort
     {
-        public static List<int> sort(List<int> list)   // complexity analysis
+        public static List<int> sort(List<int> list)            // complexity analysis  O(n^2)
         {
             for (int i = 1; i < list.Count; i++)                //n
             {
                 int key = list[i];                              //n 
                 int j = i - 1;                                  //n
-                while (j >= 0 && list[j] > key)                //n
+                while (j >= 0 && list[j] > key)                //n * n
                 {
-                    list[j + 1] = list[j];                    //n
-                    j--;                                      //n
+                    list[j + 1] = list[j];                    //n * n
+                    j--;                                      //n * n 
                 }
                 list[j + 1] = key;                           //n  
             }
